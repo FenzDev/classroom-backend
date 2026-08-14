9a6a6381f9a6d190892d169a63ea180ea2cb9cb8
FenzDev <68622361+FenzDev@users.noreply.github.com>
2026-08-14 22:29:43 +0100

    Added Cors with other slight changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import subjects from "./routes/subjects"
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+
+if (!process.env.FRONTEND_URL) throw new Error("FRONTEND_URL not specified in environment.")
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 
 app.use('/api/subjects', subjects)
 
